e85d5ceedd1de99d39ac0289cbf9288dfc9607a2
Hieu Nguyen <hieutnguyen28@gmail.com>
2017-03-14 00:30:36 -0700

    created homepage

---
+++ README.md
@@ -0,0 +1,2 @@
+#
+Use Bootstrap - Business Casual Template - Added a lot of customized css and changed the path.
---
+++ js/myscript.js
@@ -0,0 +1,36 @@
+// let log = console.log;
+
+$(document).ready(function(){
+  // <!-- Script to Activate the Carousel -->        
+  $('.carousel').carousel({
+  interval: 5000 //changes the speed
+  });
+});
+
+setInterval( function (){
+    
+    let quotebox = document.getElementById('quote');
+    let req = new XMLHttpRequest();
+
+    req.open('GET',"http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1", true);
+    req.withCredentials = true;
+    req.onload = function(){
+        if(req.status >= 200 && req.status < 400){
+            var data = JSON.parse(req.responseText);
+            renderQuote(quotebox, data);
+        }   
+    };
+    req.send();
+}, 5000);
+
+
+function renderQuote(quotebox, quote){
+  quotebox.innerHTML = '';
+  quote = quote[0].content + '~' + quote[0].title;
+  quotebox.insertAdjacentHTML('afterbegin', quote);
+}
+
+   
+
+
+
