4b08a0d8d3c0956f731f036c198c3ce8a348c1fe
Hieu <Hieu@Hieus-MacBook-Pro.local>
2017-05-27 19:09:35 -0700

    removed api called due to inappropriate content text returning

--- js/myscript.js
+++ js/myscript.js
@@ -1,33 +1,33 @@
 
-$(document).ready(function(){
-  // <!-- Script to Activate the Carousel -->        
-  $('.carousel').carousel({
-  interval: 5000 //changes the speed
-  });
-});
+// $(document).ready(function(){
+//   // <!-- Script to Activate the Carousel -->        
+//   $('.carousel').carousel({
+//   interval: 5000 //changes the speed
+//   });
+// });
 
-setInterval( function (){
+// setInterval( function (){
     
-    let quotebox = document.getElementById('quote');
-    let req = new XMLHttpRequest();
+//     let quotebox = document.getElementById('quote');
+//     let req = new XMLHttpRequest();
 
-    req.open('GET',"https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1", true);
-    req.withCredentials = true;
-    req.onload = function(){
-        if(req.status >= 200 && req.status < 400){
-            var data = JSON.parse(req.responseText);
-            renderQuote(quotebox, data);
-        }   
-    };
-    req.send();
-}, 5000);
+//     req.open('GET',"https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1", true);
+//     req.withCredentials = true;
+//     req.onload = function(){
+//         if(req.status >= 200 && req.status < 400){
+//             var data = JSON.parse(req.responseText);
+//             renderQuote(quotebox, data);
+//         }   
+//     };
+//     req.send();
+// }, 5000);
 
 
-function renderQuote(quotebox, quote){
-  quotebox.innerHTML = '';
-  quote = quote[0].content + '~' + quote[0].title;
-  quotebox.insertAdjacentHTML('afterbegin', quote);
-}
+// function renderQuote(quotebox, quote){
+//   quotebox.innerHTML = '';
+//   quote = quote[0].content + '~' + quote[0].title;
+//   quotebox.insertAdjacentHTML('afterbegin', quote);
+// }
 
    
 
